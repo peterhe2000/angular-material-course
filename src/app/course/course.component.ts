8bf943315011270522941a152a7525f1a1c330f2
Peter He <peter@mathspathway.com>
2021-10-09 15:34:52 +1100

    Angular material datatable initial

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,36 +1,53 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import { MatPaginator } from "@angular/material/paginator";
-import { MatSort } from "@angular/material/sort";
-import {Course} from "../model/course";
-import {CoursesService} from "../services/courses.service";
-import {debounceTime, distinctUntilChanged, startWith, tap, timeout} from 'rxjs/operators';
-import {merge, fromEvent} from "rxjs";
-
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  startWith,
+  tap,
+  timeout
+} from 'rxjs/operators';
+import {
+  merge,
+  fromEvent
+} from 'rxjs';
 
 @Component({
-    selector: 'course',
-    templateUrl: './course.component.html',
-    styleUrls: ['./course.component.css']
+  selector: 'course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit, AfterViewInit {
 
-    course:Course;
+  course: Course;
+  dataSource = new MatTableDataSource([]);
+  displayedColumns = ['seqNo', 'description', 'duration'];
 
-    constructor(private route: ActivatedRoute,
-                private coursesService: CoursesService) {
+  constructor(private route: ActivatedRoute,
+              private coursesService: CoursesService) {
 
-    }
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-        this.course = this.route.snapshot.data["course"];
+    this.course = this.route.snapshot.data['course'];
+    this.coursesService.findAllCourseLessons(this.course.id)
+      .subscribe(lessons => this.dataSource.data = lessons);
+  }
 
-    }
+  ngAfterViewInit() {
 
-    ngAfterViewInit() {
-
-    }
-
+  }
 
 }
