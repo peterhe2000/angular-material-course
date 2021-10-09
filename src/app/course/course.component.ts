9888dfe061c15e1b7d130212223552d77a258658
Peter He <peter@mathspathway.com>
2021-10-09 22:44:11 +1100

    Angular material use Datatable added filtering and fixed an bug which not filtering and not allow to go to 2nd page.

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -68,7 +68,7 @@ export class CourseComponent implements OnInit, AfterViewInit {
       )
       .subscribe();
 
-    merge(this.sort.sortChange, this.paginator).pipe(
+    merge(this.sort.sortChange, this.paginator.page).pipe(
       tap(() => this.loadLessonsPage()))
       .subscribe();
   }
