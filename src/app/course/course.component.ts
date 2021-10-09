9a5a524887acb1c98352a958957a39c6ac3a6340
Peter He <peter@mathspathway.com>
2021-10-09 15:47:23 +1100

    Angular material use Datatable.filter to filter text

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,27 +1,12 @@
 import {
   AfterViewInit,
   Component,
-  ElementRef,
-  OnInit,
-  ViewChild
+  OnInit
 } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  startWith,
-  tap,
-  timeout
-} from 'rxjs/operators';
-import {
-  merge,
-  fromEvent
-} from 'rxjs';
 
 @Component({
   selector: 'course',
@@ -44,6 +29,10 @@ export class CourseComponent implements OnInit, AfterViewInit {
     this.course = this.route.snapshot.data['course'];
     this.coursesService.findAllCourseLessons(this.course.id)
       .subscribe(lessons => this.dataSource.data = lessons);
+  }
+
+  searchLessons(search = '') {
+    this.dataSource.filter = search.toLowerCase().trim();
   }
 
   ngAfterViewInit() {
