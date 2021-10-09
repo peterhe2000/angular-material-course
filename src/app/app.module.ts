6f55f1a945211b670349a5f72bcf32a65053cd67
Peter He <peter@mathspathway.com>
2021-10-09 12:15:56 +1100

    Angular material Navigation and container. Use mat-card mat-icon mat-tab-group.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button'
+import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
@@ -14,13 +14,17 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        AboutComponent
+        AboutComponent,
+        TopMenuComponent,
+        CoursesCardListComponent
     ],
     imports: [
         BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,32 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {COURSES} from "../model/db-data";
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import { Course } from '../model/course';
+import { COURSES } from '../model/db-data';
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-    beginnerCourses: Course[];
+  beginnerCourses: Course[];
 
-    advancedCourses: Course[];
+  advancedCourses: Course[];
 
-    constructor() {
+  constructor() {
 
-    }
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-        const courses:any = Object.values(COURSES);
+    const courses: any = Object.values(COURSES);
 
-        this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER');
+    this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER');
 
-        this.advancedCourses = courses.filter(course => course.category === 'ADVANCED');
-    }
+    this.advancedCourses = courses.filter(course => course.category === 'ADVANCED');
+  }
 
 }
